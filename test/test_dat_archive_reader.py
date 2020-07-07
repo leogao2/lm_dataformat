c9f04c41c8f455ffefb9cbfece3ed4c6cd8b689c
Leo Gao <leogao31@gmail.com>
2020-07-07 12:40:51 -0600

    Add test

---
+++ test/test_dat_archive_reader.py
@@ -0,0 +1,19 @@
+import lm_dataformat as lmd
+
+def test_dat():
+    archive = lmd.Archive('test_dir')
+    blns = open('test/blns.txt').read()
+    archive.add_data(blns)
+    archive.add_data('testing 123')
+    archive.add_data(blns)
+    archive.add_data('testing 123456789')
+    archive.commit()
+
+    reader = lmd.Reader('test_dir')
+
+    data = list(reader.stream_data())
+
+    assert data[0] == blns
+    assert data[1] == 'testing 123'
+    assert data[2] == blns
+    assert data[3] == 'testing 123456789'
