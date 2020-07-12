6bc19257687551d5a69a16d1f9a1857d1b49fc6a
Leo Gao <leogao31@gmail.com>
2020-07-12 16:44:28 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lm_dataformat", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Leo Gao",
     author_email="leogao31@gmail.com",
     description="A utility for storing and reading files for LM training.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/leogao2/lm_dataformat",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     install_requires=[
