b1f344cd6352770086c360bb6386b751b6d53675
Tyler Thomas <36181311+tylerjthomas9@users.noreply.github.com>
2021-07-12 08:59:56 -0600

    jsonl_key -> key

--- lm_dataformat/__init__.py
+++ lm_dataformat/__init__.py
@@ -146,9 +146,9 @@ class Reader:
 
                 yield from self.read_dat(f)
             elif f.endswith('.jsonl.zst'):
-                yield from self.read_jsonl(f, get_meta, jsonl_key=key)
+                yield from self.read_jsonl(f, get_meta, key=key)
             elif f.endswith('.jsonl.zst.tar'):
-                yield from self.read_jsonl_tar(f, get_meta, jsonl_key=key)
+                yield from self.read_jsonl_tar(f, get_meta, key=key)
             elif f.endswith('.json.zst'):
                 assert not get_meta
 
